71e395c26fb3602634098e5571adcd24eb75419d
Aditya Karn <adityakarn0001@gmail.com>
2025-05-25 13:38:26 +0530

    flask api added

---
+++ flask/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/bot', methods=[ 'POST'])
+def login():
+   data=request.json
+   
+   
+   return jsonify({"data": data, "message": "Data received successfully"}), 200
+
+
+
+
+if __name__ == '__main__':  
+   app.run(debug=True)  
