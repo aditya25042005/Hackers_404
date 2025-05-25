fd2407d33de2f66b7b969549dad33336e146138e
Aditya-Gupta23 <9555.adityagupta@gmail.com>
2025-05-25 21:21:14 +0530

    Improved Chat

--- Frontend/CertifyoHackathon/src/context/UserContext.tsx
+++ Frontend/CertifyoHackathon/src/context/UserContext.tsx
@@ -1,31 +1,49 @@
-import { createContext,useContext,useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 import { type ReactNode } from "react";
+import axios from "axios";
 
 
-interface User{
-    email:string
+interface User {
+    email: string
 }
 
-interface UserContextType{
-    user:User|null;
-    setUser:(user:User|null)=>void;
+interface UserContextType {
+    user: User | null;
+    setUser: (user: User | null) => void;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
 
 
-export const UserProvider=({children}:{children:ReactNode})=>{
+export const UserProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
 
-    return(
-       < UserContext.Provider value={{ user, setUser }}>
-       {children}
-       </UserContext.Provider>
-    );
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const res = await axios.get("http://localhost:8000/user/give_email", {
+                    withCredentials: true,
+                });
+                if (res.data.email) {
+                    setUser({ email: res.data.email });
+                }
+            } catch (err) {
+                console.log("Error fetching user:", err);
+            }
+        };
+
+        fetchUser();
+    }, []);
+
+return (
+    < UserContext.Provider value={{ user, setUser }}>
+        {children}
+    </UserContext.Provider>
+);
 };
 
-export const useUser =() =>{
-    const context=useContext(UserContext)
+export const useUser = () => {
+    const context = useContext(UserContext)
     if (!context) throw new Error("useUser must be used within a UserProvider");
-  return context;
+    return context;
 }
