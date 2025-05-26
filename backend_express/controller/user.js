7a8eea3468e85de14ecfbde07a26b14194c6eed1
Aditya Karn <adityakarn0001@gmail.com>
2025-05-26 22:29:58 +0530

    cookissue

--- backend_express/controller/user.js
+++ backend_express/controller/user.js
@@ -149,7 +149,8 @@ const token=jwt.sign({id:user._id,email:user.email},"aditya",{expiresIn:"1d"})
 console.log(token)
 res.cookie('token', token, {
   httpOnly: true,
-  sameSite: 'None',
+    sameSite: 'None',
+
   secure: true,  // always send cookie only over HTTPS
   maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
 });
